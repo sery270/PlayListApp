325a0996c291102aebf9871c6cc27416446a1083
sery270 <sery270@naver.com>
2021-06-30 12:17:31 +0900

    [CHORE] base 코드 작성

--- app/src/main/java/com/example/playlistapp/base/BindingFragment.kt
+++ app/src/main/java/com/example/playlistapp/base/BindingFragment.kt
@@ -1,4 +1,45 @@
 package com.example.playlistapp.base
 
-class BindingFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import timber.log.Timber
+
+abstract class BindingFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    protected inner class LifeCycleEventLogger(private val className: String) : LifecycleObserver {
+        fun registerLogger(lifecycle: Lifecycle) {
+            lifecycle.addObserver(this)
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+        fun log() {
+            Timber.d("${lifecycle.currentState}")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
