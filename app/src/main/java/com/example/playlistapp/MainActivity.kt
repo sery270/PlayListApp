d1d710475ab492503874716b8af3d912eeb8ede5
sery270 <sery270@naver.com>
2021-07-07 15:13:45 +0900

    [CHORE] 코드 정리

--- app/src/main/java/com/example/playlistapp/MainActivity.kt
+++ app/src/main/java/com/example/playlistapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.playlistapp
 
-import android.os.Bundle
 import com.example.playlistapp.base.BindingActivity
 import com.example.playlistapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
