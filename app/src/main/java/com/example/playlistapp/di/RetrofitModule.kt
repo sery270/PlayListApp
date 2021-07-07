1953c8a0755765d80f5215779efaf8bc7e8b4201
sery270 <sery270@naver.com>
2021-07-07 12:09:10 +0900

    [REFACTOR] rx로 retrofit 객체 수정 (DI 수정)

--- app/src/main/java/com/example/playlistapp/di/RetrofitModule.kt
+++ app/src/main/java/com/example/playlistapp/di/RetrofitModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,7 +17,11 @@ object RetrofitModule {
     @Provides
     @Singleton
     fun provideRetrofitObject(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        return Retrofit.Builder()
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
     }
 }
